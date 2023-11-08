3530185852f564e0b5b58ec07bbe46f367d55b1e
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-11-08 10:25:06 -0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
+import { ToDoForm } from './ToDoForm';
+import { ToDoList } from './ToDoList';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
